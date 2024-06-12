5b039f8f27c3586b07470bd2b3a6d79394b51b0b
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-06-12 11:06:58 -0300

    primer commit - reComiendo App - Back

---
+++ src/main/java/com/recomiendo/recomiendo_app/RecomiendoAppApplication.java
@@ -0,0 +1,13 @@
+package com.recomiendo.recomiendo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomiendoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomiendoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recomiendo/recomiendo_app/RecomiendoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recomiendo.recomiendo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomiendoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

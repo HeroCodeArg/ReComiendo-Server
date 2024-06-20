12bc6c6ae6259968c4fe21d8cc7cfbe657d30e22
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-06-19 21:12:47 -0300

    Borre comentarios innecesarios.

--- src/main/java/com/recomiendo/recomiendo_app/RecomiendoAppApplication.java
+++ src/main/java/com/recomiendo/recomiendo_app/RecomiendoAppApplication.java
@@ -3,18 +3,11 @@ package com.recomiendo.recomiendo_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/* import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase; */
-
 @SpringBootApplication
 public class RecomiendoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecomiendoAppApplication.class, args);
-		/* MongoClient mongoClient = MongoClients.create();
-        MongoDatabase db = mongoClient.getDatabase("reComiendoDB");
-		System.out.println("DB users: "+db.getCollection("users")); */
 	}
 
 }
